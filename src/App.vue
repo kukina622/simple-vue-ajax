a2ea644865d78935c58d2d8dd72c1347636b0a63
Min <wyes91622@gmail.com>
2023-03-11 22:48:46 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+const products = ref([]);
+
+async function fetchAPI() {
+  const res = await axios.get("https://dummyjson.com/products");
+  products.value = res.data.products;
+}
+
+</script>
+
+<template>
+  <button @click="fetchAPI">fetch api</button>
+  <p>{{ products[0] }}</p>
+</template>
